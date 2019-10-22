189ed698432632bd6b41119a2be34f20324197dc
Matt C <mcompton2002@gmail.com>
2019-10-22 19:52:06 -0400

    Update dev-deploy script

--- server_run.bat
+++ server_run.bat
@@ -1,1 +1,3 @@
-ssh matt@10.0.0.7 "rm -rfv VenturyBoi && git clone https://github.com/kketg/VenturyBoi && cd VenturyBoi && python3 app.py"
+ssh matt@10.0.0.7 "rm -rfv VenturyBoi"
+ssh matt@10.0.0.7 "git clone https://github.com/kketg/VenturyBoi"
+ssh matt@10.0.0.7 "cd VenturyBoi && python3 -m pip install -r requirements.txt && python3 app.py"
