f021bdc7f4e35bb942807183e51f25ec4f88f8fa
Matt C <mcompton2002@gmail.com>
2019-10-22 19:59:20 -0400

    auto-commit for dev-test (sorry if broke)

--- server_run.bat
+++ server_run.bat
@@ -1,4 +1,7 @@
+git add *
+git commit -m "auto-commit for dev-test (sorry if broke)"
+git push
+
 ssh matt@10.0.0.7 "rm -rfv VenturyBoi"
 ssh matt@10.0.0.7 "git clone https://github.com/kketg/VenturyBoi"
-ssh matt@10.0.0.7 "cd VenturyBoi && python3 -m pip install -r requirements.txt"
 ssh matt@10.0.0.7 "cd VenturyBoi && python3 app.py"
