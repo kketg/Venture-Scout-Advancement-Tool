98ed16a923132951670ce22d14e4d1921e8e0fdc
Matt C <mcompton2002@gmail.com>
2019-10-22 20:07:58 -0400

    Auto-kill any old server

--- server_run.bat
+++ server_run.bat
@@ -4,5 +4,7 @@ git push
 
 ssh matt@10.0.0.7 "rm -rfv VenturyBoi"
 ssh matt@10.0.0.7 "git clone https://github.com/kketg/VenturyBoi"
+ssh matt@10.0.0.7 "kill $( lsof -i:9090 -t )"
+
 ssh matt@10.0.0.7 "cd VenturyBoi && python3 app.py"
 pause
