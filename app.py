bc7f0766b33986ce025d6c197587abe1358801c4
Matt C <mcompton2002@gmail.com>
2021-02-25 12:15:25 -0500

    Removed stuff we didn't use

--- app.py
+++ app.py
@@ -261,9 +261,6 @@ def getinfos(rank, requirement):
 
 if __name__ == "__main__":
 
-    # Run black linter to ficc any bad code
-    os.system("python lint.py")
-
     db.addscout("dummyscout", "samplepassword", "John Doe")
 
     app.run(host="0.0.0.0", port="9090", debug=True)
--- lint.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for file in os.listdir("."):
-    if ".py" in file:
-        os.system("black " + file)
-
-# os.system("java -jar vnu.jar templates/")
